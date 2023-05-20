dbb3457a0250407e811bc93427a5a1abc445283d
Erik Alves <dev.eriksantos@gmail.com>
2023-05-20 18:18:10 -0300

    Include component profile and styles css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import Profile from "./components/Profile/index"
-import imgPerfil from "./assets/myPhoto.png"
+import Profile from "./components/profile/index"
+import imgPerfil01 from "./assets/myPhoto.png"
 
 export default function App() {
   return (
       <>
-          <Profile imgPerfil={imgPerfil} />
+        <Profile imgPerfil={imgPerfil01}
+          name="Erik Alves"
+          bio="FullStack Developer"
+          contact="(89) 99430-7093"
+          email="dev.eriksantos@gmail.com"
+          githubUrl="https://github.com/dev-erikalves"
+          linkedinUrl="https://www.linkedin.com/in/dev-erikalves/"
+          instagramUrl="https://www.instagram.com/developerik.web/"
+        />
       </>
     )
 }
--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -1,13 +1,18 @@
+import styles from "./styles.module.css"
+
 export default function Profile(props) {
-    const myNumber = "(89) 99430-7093"
-    const myEmail = "dev.eriksantos@gmail.com"
     return (
-        <div className="container-profile">
-            <img src={props.imgPerfil} alt="Foto de Perfil" />
-            <h3>Erik Alves</h3>
-            <p>FullStack Developer</p>
-            <p>{myNumber}</p>
-            <p>{myEmail}</p>
-        </div>    
+        <div className={styles.container}>
+            <img className={styles.myPhoto} src={props.imgPerfil} alt="Foto de Perfil" />
+            <h3>{props.name}</h3>
+            <p>{props.bio}</p>
+            <p>{props.contact}</p>
+            <p>{props.email}</p>
+            <div className={styles.containerButtons}>
+                <a href={props.githubUrl} target="_blank">Github</a>
+                <a href={props.linkedinUrl} target="_blank">Linkedin</a>
+                <a href={props.instagramUrl} target="_blank">Instagram</a>
+            </div>
+        </div>
     )
 }
